e73f69c37bde4931cd44bc77aade7d3b71d7b03e
julian <sythang11@gmail.com>
2018-07-18 17:04:41 +0700

    Added root page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  get 'home/index'
   devise_for :users
+  root 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   mount Api::Api => "/"
   mount GrapeSwaggerRails::Engine => '/documentation'
